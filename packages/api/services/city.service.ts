5e020d5f30bcd1770c47031208137c08900c7eff
Nnaemeka Ezechi <nnaemeka@lifebit.ai>
2024-12-17 15:13:39 +0100

    feat: implement getCityById method

---
+++ packages/api/services/city.service.ts
@@ -0,0 +1,23 @@
+import { ObjectId } from "mongodb";
+
+import { City } from "../types";
+import { getCollection } from "../config/database";
+import { AvailableCollections } from "../enums";
+import { NotFoundError } from "../utils/errors";
+
+export const getCityById = async (id: string): Promise<City> => {
+  if (!ObjectId.isValid(id)) {
+    throw new NotFoundError("Invalid city ID");
+  }
+  const city = await getCollection<City>(AvailableCollections.Cities).findOne({ _id: new ObjectId(id) });
+
+  if (!city) {
+    throw new NotFoundError("City not found");
+  }
+
+  return city;
+};
+
+export const getAllCities = async (): Promise<City[]> => {
+  return getCollection<City>(AvailableCollections.Cities).find({}).toArray();
+};
