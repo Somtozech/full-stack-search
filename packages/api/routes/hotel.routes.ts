c7623a5874186fc4d98861a4d8126f479e0e4305
Nnaemeka Ezechi <nnaemeka@lifebit.ai>
2024-12-17 15:05:31 +0100

    feat: setup hotel routes

---
+++ packages/api/routes/hotel.routes.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { validateId } from "../middlewares/validation";
+
+import { getHotelById } from "services/hotel.service";
+
+export const hotelRoutes = Router();
+
+hotelRoutes.get("/:id", validateId, async (req, res, next) => {
+  const hotel = await getHotelById(req.params.id);
+
+  res.json({
+    status: "success",
+    data: hotel,
+  });
+});
--- packages/api/routes/index.ts
+++ packages/api/routes/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
 
 import { searchRoutes } from "./search.routes";
+import { hotelRoutes } from "./hotel.routes";
 
 export const apiRouter = express.Router();
 
 apiRouter.use("/search", searchRoutes);
+apiRouter.use("/hotels", hotelRoutes);
