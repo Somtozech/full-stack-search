0e89abd6bcc7e3792e41a65d6dfd1b6c71a9ad5d
Nnaemeka Ezechi <nnaemeka@lifebit.ai>
2024-12-17 15:14:52 +0100

    feat: setup contry routes

---
+++ packages/api/routes/country.routes.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+
+import { getCountryById } from "services/country.service";
+
+export const countryRoutes = Router();
+
+countryRoutes.get("/:id", async (req, res, next) => {
+  const country = await getCountryById(req.params.id);
+  res.json(country);
+
+  res.json({
+    status: "success",
+    data: country,
+  });
+});
--- packages/api/routes/index.ts
+++ packages/api/routes/index.ts
@@ -3,9 +3,11 @@ import express from "express";
 import { searchRoutes } from "./search.routes";
 import { hotelRoutes } from "./hotel.routes";
 import { cityRoutes } from "./city.routes";
+import { countryRoutes } from "./country.routes";
 
 export const apiRouter = express.Router();
 
 apiRouter.use("/search", searchRoutes);
 apiRouter.use("/hotels", hotelRoutes);
 apiRouter.use("/cities", cityRoutes);
+apiRouter.use("/countries", countryRoutes);
