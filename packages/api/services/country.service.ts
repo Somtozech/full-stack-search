747e93ef4b0198e4fb9aa8e2304ead7ad1f9ef4b
Nnaemeka Ezechi <nnaemeka@lifebit.ai>
2024-12-17 15:14:16 +0100

    feat: implement getCountryById method

---
+++ packages/api/services/country.service.ts
@@ -0,0 +1,19 @@
+import { ObjectId } from "mongodb";
+import { Country } from "../types";
+import { getCollection } from "../config/database";
+import { AvailableCollections } from "../enums";
+import { NotFoundError } from "../utils/errors";
+
+export const getCountryById = async (id: string): Promise<Country> => {
+  if (!ObjectId.isValid(id)) {
+    throw new NotFoundError("Invalid country ID");
+  }
+
+  const country = await getCollection<Country>(AvailableCollections.Countries).findOne({ _id: new ObjectId(id) });
+
+  if (!country) {
+    throw new NotFoundError("Country not found");
+  }
+
+  return country;
+};
