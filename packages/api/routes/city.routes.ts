44b96c9c3baeacf27168d56ff6c4b3a953cd6bd6
Nnaemeka Ezechi <nnaemeka@lifebit.ai>
2024-12-17 15:13:57 +0100

    feat: setup city routes

---
+++ packages/api/routes/city.routes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { getCityById } from "../services/city.service";
+
+export const cityRoutes = Router();
+
+cityRoutes.get("/:id", async (req, res, next) => {
+  const city = await getCityById(req.params.id);
+  res.json(city);
+
+  res.json({
+    status: "success",
+    data: city,
+  });
+});
--- packages/api/routes/index.ts
+++ packages/api/routes/index.ts
@@ -2,8 +2,10 @@ import express from "express";
 
 import { searchRoutes } from "./search.routes";
 import { hotelRoutes } from "./hotel.routes";
+import { cityRoutes } from "./city.routes";
 
 export const apiRouter = express.Router();
 
 apiRouter.use("/search", searchRoutes);
 apiRouter.use("/hotels", hotelRoutes);
+apiRouter.use("/cities", cityRoutes);
